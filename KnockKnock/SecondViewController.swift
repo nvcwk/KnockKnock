6afc3a37e35150758bac721be2e8a97312062281
Nicholas Chan <nicholasvinchanwk@msn.com>
2015-12-04 14:56:59 +0800

    Initial Commit

---
+++ KnockKnock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KnockKnock
+//
+//  Created by Nicholas Chan on 4/12/15.
+//  Copyright © 2015 Gen6. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
