edc7466d5fec2e0b56f8b87b64df04e90426c862
Nvcwk <nicholasvinchanwk@msn.com>
2015-12-09 03:39:46 +0800

    Included FB Backend

--- KnockKnock/AppDelegate.swift
+++ KnockKnock/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import ParseFacebookUtilsV4
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
        
         ParseUtils.loadParse()
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
                
         return true
     }
@@ -38,12 +41,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?,
+        annotation: AnyObject?) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(application,
+                openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 }
 
