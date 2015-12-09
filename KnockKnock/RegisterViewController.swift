647062d7218b3c55d894b32c6e97bef3e9f4c979
Nvcwk <nicholasvinchanwk@msn.com>
2015-12-10 00:17:26 +0800

    Function Registration

--- KnockKnock/RegisterViewController.swift
+++ KnockKnock/RegisterViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import CountryPicker
+import SwiftValidator
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, CountryPickerDelegate, ValidationDelegate {
     
     @IBOutlet weak var tf_country: UITextField!
     @IBOutlet weak var tf_email: UITextField!
@@ -20,27 +22,56 @@ class RegisterViewController: UIViewController {
     
     var selectedDate = NSDate()
     
-    //let countryPickerView = CountryPicker()
+    let countryPickerView = CountryPicker()
     let datePicker = UIDatePicker()
+    
+    let validator = Validator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
+        countryPickerView.delegate = self
+        countryPickerView.selectedLocale = NSLocale.currentLocale()
+        tf_country.inputView = countryPickerView
+        tf_country.text = countryPickerView.selectedCountryName
+        
         datePicker.datePickerMode = UIDatePickerMode.Date
         datePicker.maximumDate = NSDate()
         datePicker.addTarget(self, action: Selector("updateBirthday:"),
             forControlEvents:UIControlEvents.ValueChanged)
         tf_birthday.inputView = datePicker
+        
+        let missTxt = String("Please fill in all inputs")
+        
+        validator.registerField(tf_email, rules: [RequiredRule(message: missTxt), EmailRule(message: "Invalid email")])
+        validator.registerField(tf_pass, rules: [RequiredRule(message: missTxt), ConfirmationRule(confirmField: tf_cfmPass, message: "Password do not match!")])
+        validator.registerField(tf_username, rules: [RequiredRule(message: missTxt)])
+        validator.registerField(tf_country, rules: [RequiredRule(message: missTxt)])
+        validator.registerField(tf_birthday, rules: [RequiredRule(message: missTxt)])
+        validator.registerField(tf_contact, rules: [RequiredRule(message: missTxt)])
     }
     
-        func updateBirthday(sender: UIDatePicker) {
-            tf_birthday.text = KnockKnockUtils.dateToString(sender.date)
-            selectedDate = sender.date
-        }
+    func updateBirthday(sender: UIDatePicker) {
+        tf_birthday.text = KnockKnockUtils.dateToString(sender.date)
+        selectedDate = sender.date
+    }
+    
+    func countryPicker(picker: CountryPicker!, didSelectCountryWithName name: String!, code: String!) {
+        tf_country.text = name
+    }
     
     @IBAction func actionSignUser(sender: UIButton) {
-        print("heello")
+        validator.validate(self)
     }
+    
+    func validationSuccessful() {
+       ParseUtils.signUp(self, email: tf_email.text!, username: tf_username.text!, password: tf_pass.text!, country: tf_country.text!, contact: Int(tf_contact.text!)!, birthday: selectedDate)
+    }
+    
+    func validationFailed(errors:[UITextField:ValidationError]) {
+        KnockKnockUtils.okAlert(self, title: "Error", message: (errors.values.first?.errorMessage)!, handle: nil)
+    }
+
 }
---
+++ Pods/Target Support Files/CountryPicker/CountryPicker-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "CountryPicker.h"
+
+FOUNDATION_EXPORT double CountryPickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CountryPickerVersionString[];
+
