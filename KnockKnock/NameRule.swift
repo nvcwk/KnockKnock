138b893b8375f5b964f65e34d09c3f09695ec4ad
Nvcwk <nicholasvinchanwk@msn.com>
2015-12-12 19:30:18 +0800

    More Validation for First and Last Name

---
+++ KnockKnock/NameRule.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftValidator
+
+public class NameRule : Rule {
+    
+    private var REGEX: String = "^(?=.*?[A-Z]).{8,}$"
+    private var message : String
+    
+    public init(regex: String, message: String = "Invalid Regular Expression"){
+        self.REGEX = regex
+        self.message = message
+    }
+    
+    public func validate(value: String) -> Bool {
+        let match = try! NSRegularExpression(pattern: REGEX, options: [.CaseInsensitive])
+        
+        let range = NSMakeRange(0, value.characters.count)
+        
+        let matchRange = match.rangeOfFirstMatchInString(value, options: .ReportProgress, range: range)
+        
+        let valid = matchRange.location != NSNotFound
+        
+        return valid
+    }
+    
+    public func errorMessage() -> String {
+        return message
+    }
+}
