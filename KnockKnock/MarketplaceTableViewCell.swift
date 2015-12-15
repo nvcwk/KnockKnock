7fc9946955bfd471c39dc9762e02c2abea4ebf30
Don <don.teo.2013@sis.smu.edu.sg>
2015-12-15 19:33:39 +0800

    Beta one of marketplace

---
+++ KnockKnock/MarketplaceTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MarketplaceTableViewCell.swift
+//  KnockKnock
+//
+//  Created by Don Teo on 15/12/15.
+//  Copyright © 2015 Gen6. All rights reserved.
+//
+
+import UIKit
+
+class MarketplaceTableViewCell: UITableViewCell {
+    @IBOutlet weak var bkimageview: UIImageView!
+    @IBOutlet weak var headingLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
