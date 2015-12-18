82df4e65a6cffa59ee39c663fa95bb0e856bd497
Nvcwk <nicholasvinchanwk@msn.com>
2015-12-18 21:11:38 +0800

    Itinerary Summary

---
+++ KnockKnock/ItiMainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ItineraryViewController.swift
+//  KnockKnock
+//
+//  Created by Nicholas Chan on 18/12/15.
+//  Copyright © 2015 Gen6. All rights reserved.
+//
+
+import UIKit
+
+class ItiMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backItinerary(segue:UIStoryboardSegue) {
+        
+    }
+    
+
+    
+
+
+}
