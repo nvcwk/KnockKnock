e3e9f1b4249970d49e61cae72a9f64a4ef8a6aed
Don <don.teo.2013@sis.smu.edu.sg>
2015-12-28 13:57:05 +0800

    Included activity cell

---
+++ KnockKnock/ActivityTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ActivityTableViewCell.swift
+//  KnockKnock
+//
+//  Created by Don Teo on 27/12/15.
+//  Copyright © 2015 Gen6. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+import ParseUI
+
+class ActivityTableViewCell: PFTableViewCell {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
