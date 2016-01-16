0b862e1958afa41a3f0217fc7164d5c85241e200
Nvcwk <nicholasvinchanwk@msn.com>
2016-01-16 12:55:33 +0800

    Improved MarketPlaceDetails

--- KnockKnock/MarketPlaceV2TableViewController.swift
+++ KnockKnock/MarketPlaceV2TableViewController.swift
@@ -11,8 +11,11 @@ import Parse
 import ParseUI
 
 class MarketPlaceV2TableViewController: PFQueryTableViewController {
-
+    
+    var published = true
+    
     override func viewDidLoad() {
+        print("TEST")
         super.viewDidLoad()
         
     }
@@ -21,17 +24,17 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
     override func queryForTable() -> PFQuery {
         var query = PFQuery(className: "MarketPlace")
         
-        query.whereKey("isPublished", equalTo: true)
+        query.whereKey("isPublished", equalTo: published)
         query.whereKey("host", notEqualTo: PFUser.currentUser()!)
         
         query.includeKey("itinerary")
         query.includeKey("host")
+        query.includeKey("itinerary.activities")
         
         return query
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell {
-        
         var cell = tableView.dequeueReusableCellWithIdentifier("marketplaceCell") as! MarketPlaceV2TableViewCell
         
         if let mpObj = object {
@@ -39,9 +42,9 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
             let itiObj = mpObj["itinerary"] as! PFObject
             
             cell.lb_title.text = itiObj["title"] as! String
-
+            
             cell.lb_price.text = String(mpObj["price"] as! Int)
-
+            
             let image = itiObj["image"] as! PFFile
             
             cell.image_background.file = image
@@ -54,6 +57,29 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 163.0
     }
-
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "toMarketPlaceDetail") {
+            if let cell = sender as? UITableViewCell {
+                let indexPath = tableView.indexPathForCell(cell)
+    
+                let controller = segue.destinationViewController as! MarketPlaceDetailsV2ViewController
+                
+                let pubObj = objectAtIndexPath(indexPath)
+                
+                let itiObj = pubObj!["itinerary"] as! PFObject
+                
+                let hostObj = pubObj!["host"] as! PFUser
+                
+                controller.pubObj = pubObj!
+                controller.itiObj = itiObj
+                controller.hostObj = hostObj
+    
+            }
+            
+        }
+    }
+    
+    
+    
 }
