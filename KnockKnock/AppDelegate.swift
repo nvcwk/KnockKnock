4b95b7618d055c8afd2ede66c84b85ed2ac69190
Nvcwk <nicholasvinchanwk@msn.com>
2016-01-22 20:54:03 +0800

    Switch DB

--- KnockKnock/AppDelegate.swift
+++ KnockKnock/AppDelegate.swift
@@ -22,9 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
        
-        GMSServices.provideAPIKey("AIzaSyCU2X9erRL1NBnrHOSN6mq7Qq2qHhsVOWQ")
+        //GMSServices.provideAPIKey("AIzaSyCU2X9erRL1NBnrHOSN6mq7Qq2qHhsVOWQ")
         ParseUtils.loadParse()
-        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+        //PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
         
         
         
