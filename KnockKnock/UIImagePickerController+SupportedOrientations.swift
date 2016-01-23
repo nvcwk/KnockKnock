f72ad09be98d9ea52d489162e8f8bfb03feb6d1b
Nvcwk <nicholasvinchanwk@msn.com>
2016-01-23 13:11:33 +0800

    Extension Added to force landscape camera

---
+++ KnockKnock/UIImagePickerController+SupportedOrientations.swift
@@ -0,0 +1,23 @@
+//
+//  UIImagePickerController+SupportedOrientations.swift
+//  KnockKnock
+//
+//  Created by Nicholas Chan on 23/1/16.
+//  Copyright © 2016 Gen6. All rights reserved.
+//
+import UIKit
+
+extension UIImagePickerController
+{
+    public override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Landscape
+    }
+}
+
+
+class LandscapePickerController: UIImagePickerController
+{
+    public override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Landscape
+    }
+}
