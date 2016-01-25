483f49f149dc58ba6d6eaa89a971d03f7060c6a8
Don <don.teo.2013@sis.smu.edu.sg>
2016-01-25 16:14:04 +0800

    Review button now dynamic

---
+++ KnockKnock/RatingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RatingViewController.swift
+//  KnockKnock
+//
+//  Created by Don Teo on 25/1/16.
+//  Copyright © 2016 Gen6. All rights reserved.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
