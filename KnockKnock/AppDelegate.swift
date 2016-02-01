d7bc54b32f1b65ef5e2112db2241ba976c9c8d63
Nvcwk <nicholasvinchanwk@msn.com>
2016-02-01 15:49:39 +0800

    Remove unnecessary things

--- KnockKnock/AppDelegate.swift
+++ KnockKnock/AppDelegate.swift
@@ -61,17 +61,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        FBSDKAppEvents.activateApp()
+//        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?,
-        annotation: AnyObject?) -> Bool {
-            return FBSDKApplicationDelegate.sharedInstance().application(application,
-                openURL: url, sourceApplication: sourceApplication, annotation: annotation)
-    }
+//    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?,
+//        annotation: AnyObject?) -> Bool {
+//            return FBSDKApplicationDelegate.sharedInstance().application(application,
+//                openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+//    }
 }
 
