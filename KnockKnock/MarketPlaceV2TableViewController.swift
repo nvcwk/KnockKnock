42a633bf8dd8b15d1ef0655c9d2c03c8d89ba17a
Nvcwk <nicholasvinchanwk@msn.com>
2016-02-01 18:07:26 +0800

    Resolve marketplace

--- KnockKnock/MarketPlaceV2TableViewController.swift
+++ KnockKnock/MarketPlaceV2TableViewController.swift
@@ -29,14 +29,17 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
     // Define the query that will provide the data for the table view
     override func queryForTable() -> PFQuery {
         var query = PFQuery(className: "MarketPlace")
+        query.includeKey("itinerary")
+        query.includeKey("host")
+        query.includeKey("itinerary.activities")
         
-//        query.whereKey("isPublished", equalTo: published)
-//        //query.whereKey("host", notEqualTo: PFUser.currentUser()!)
-//        
-//        query.includeKey("itinerary")
-//        query.includeKey("host")
-//        query.includeKey("itinerary.activities")
-
+        //        query.whereKey("isPublished", equalTo: published)
+        //        //query.whereKey("host", notEqualTo: PFUser.currentUser()!)
+        //
+        //        query.includeKey("itinerary")
+        //        query.includeKey("host")
+        //        query.includeKey("itinerary.activities")
+        
         if(PFUser.currentUser() != nil) {
             if (sort == 0) {
                 query.addDescendingOrder("price")
@@ -49,11 +52,6 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
             query.whereKey("isPublished", equalTo: published)
             query.whereKey("host", notEqualTo: PFUser.currentUser()!)
             query.whereKey("lastAvailability", greaterThanOrEqualTo: NSDate())
-            
-            query.includeKey("itinerary")
-            query.includeKey("host")
-            query.includeKey("itinerary.activities")
-            
         }
         
         return query
@@ -64,47 +62,48 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
         
         if let mpObj = object {
             
-            let itiObj = mpObj["itinerary"] as! PFObject
-            
-            cell.lb_title.text = itiObj["title"] as! String
-            
-            cell.lb_price.text = String(mpObj["price"] as! Int) + " SGD"
-            
-            cell.startDate.text = KnockKnockUtils.dateToStringDisplay( mpObj["startAvailability"] as! NSDate)
-            
-            
-            cell.endDate.text = KnockKnockUtils.dateToStringDisplay( mpObj["lastAvailability"] as! NSDate)
-            let image = itiObj["image"] as! PFFile
-            
-            cell.image_background.file = image
-            cell.image_background.loadInBackground()
-            
-            
-           //for stars
-            let hostObj = itiObj["host"] as! PFObject
-            var rating : Double = 0.0
-            var ratingCount = 0.0
-            
-            let query = PFQuery(className: "Review")
-            query.whereKey("Itinerary", equalTo: itiObj)
-            query.whereKey("ClientReview", equalTo: true)
-            query.findObjectsInBackgroundWithBlock {
-                (objects: [PFObject]?, error: NSError?) -> Void in
+            if let itiObj = mpObj["itinerary"] as? PFObject {
                 
-                if error == nil{
-                    if let objects = objects as [PFObject]!{
-                        for object in objects {
-                            if (object["Stars"] != nil){
-                                rating = rating +  Double(object["Stars"] as! Double)
-                                ratingCount = ratingCount + 1
+                cell.lb_title.text = itiObj["title"] as! String
+                
+                cell.lb_price.text = String(mpObj["price"] as! Int) + " SGD"
+                
+                cell.startDate.text = KnockKnockUtils.dateToStringDisplay( mpObj["startAvailability"] as! NSDate)
+                
+                
+                cell.endDate.text = KnockKnockUtils.dateToStringDisplay( mpObj["lastAvailability"] as! NSDate)
+                let image = itiObj["image"] as! PFFile
+                
+                cell.image_background.file = image
+                cell.image_background.loadInBackground()
+                
+                
+                //for stars
+                let hostObj = itiObj["host"] as! PFObject
+                var rating : Double = 0.0
+                var ratingCount = 0.0
+                
+                let query = PFQuery(className: "Review")
+                query.whereKey("Itinerary", equalTo: itiObj)
+                query.whereKey("ClientReview", equalTo: true)
+                query.findObjectsInBackgroundWithBlock {
+                    (objects: [PFObject]?, error: NSError?) -> Void in
+                    
+                    if error == nil{
+                        if let objects = objects as [PFObject]!{
+                            for object in objects {
+                                if (object["Stars"] != nil){
+                                    rating = rating +  Double(object["Stars"] as! Double)
+                                    ratingCount = ratingCount + 1
+                                }
+                                var stars = rating/ratingCount
+                                cell.stars.value = CGFloat(stars)
                             }
-                            var stars = rating/ratingCount
-                            cell.stars.value = CGFloat(stars)
                         }
+                    }else{
+                        //log details of the failure
+                        print("error: \(error!)  \(error!.userInfo)")
                     }
-                }else{
-                    //log details of the failure
-                    print("error: \(error!)  \(error!.userInfo)")
                 }
             }
             
@@ -114,15 +113,15 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
         return cell
     }
     
-//    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        return 308.0
-//    }
+    //    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    //        return 308.0
+    //    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if(segue.identifier == "toMarketPlaceDetail") {
             if let cell = sender as? UITableViewCell {
                 let indexPath = tableView.indexPathForCell(cell)
-    
+                
                 let controller = segue.destinationViewController as! MarketPlaceDetailsViewController_V2
                 
                 let pubObj = objectAtIndexPath(indexPath)
@@ -134,7 +133,7 @@ class MarketPlaceV2TableViewController: PFQueryTableViewController {
                 controller.pubObj = pubObj!
                 controller.itiObj = itiObj
                 controller.hostObj = hostObj
-    
+                
             }
             
         }
