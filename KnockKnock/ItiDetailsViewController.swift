ac3265b7508c6d7b59cbe059dcdd50cb77f5d21f
Nvcwk <nicholasvinchanwk@msn.com>
2016-02-13 01:54:49 +0800

    Iti & Pub Fullscreen

--- KnockKnock/ItiDetailsViewController.swift
+++ KnockKnock/ItiDetailsViewController.swift
@@ -17,7 +17,7 @@ class ItiDetailsViewController: UIViewController {
     
     @IBOutlet weak var lb_title: UILabel!
     @IBOutlet weak var tv_description: UITextView!
-//    @IBOutlet weak var image_image: PFImageView!
+    //    @IBOutlet weak var image_image: PFImageView!
     @IBOutlet weak var tv_activities: UITableView!
     @IBOutlet weak var barBtn_publish: UIBarButtonItem!
     @IBOutlet weak var profile_img: PFImageView!
@@ -27,6 +27,8 @@ class ItiDetailsViewController: UIViewController {
     var activities = NSArray()
     
     @IBOutlet weak var slideshow_images: ImageSlideshow!
+    
+    var transitionDelegate: ZoomAnimatedTransitioningDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,8 +67,8 @@ class ItiDetailsViewController: UIViewController {
         
         lb_title.text = itineraryObj["title"] as! String
         
-//        image_image.file = itineraryObj["image"] as! PFFile
-//        image_image.loadInBackground()
+        //        image_image.file = itineraryObj["image"] as! PFFile
+        //        image_image.loadInBackground()
         
         //        tv_description.text = itineraryObj["summary"] as! String
         
@@ -98,6 +100,27 @@ class ItiDetailsViewController: UIViewController {
         slideshow_images.clipsToBounds = true
         slideshow_images.contentScaleMode = UIViewContentMode.ScaleAspectFill
         slideshow_images.setImageInputs(imageArr)
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "openFullScreen")
+        slideshow_images.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    
+    func openFullScreen() {
+        let ctr = FullScreenSlideshowViewController()
+        // called when full-screen VC dismissed and used to set the page to our original slideshow
+        ctr.pageSelected = {(page: Int) in
+            self.slideshow_images.setScrollViewPage(page, animated: false)
+        }
+        
+        // set the initial page
+        ctr.initialPage = slideshow_images.scrollViewPage
+        // set the inputs
+        ctr.inputs = slideshow_images.images
+        self.transitionDelegate = ZoomAnimatedTransitioningDelegate(slideshowView: slideshow_images);
+        ctr.transitioningDelegate = self.transitionDelegate!
+        
+        self.presentViewController(ctr, animated: true, completion: nil)
     }
     
     
@@ -108,6 +131,7 @@ class ItiDetailsViewController: UIViewController {
             controller.itineraryObj = itineraryObj
         }
     }
+    
 }
 
 extension ItiDetailsViewController : UITableViewDelegate, UITableViewDataSource {
