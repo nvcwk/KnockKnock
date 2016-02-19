1cd3541a80aaaf1dce132e7b3a26775fd073ed4b
Nvcwk <nicholasvinchanwk@msn.com>
2016-02-19 16:49:18 +0800

    Use old sort for next release

--- KnockKnock/MarketPlaceV2ViewController.swift
+++ KnockKnock/MarketPlaceV2ViewController.swift
@@ -11,12 +11,32 @@ import QIULaunchAnimation
 import autoAutoLayout
 import Parse
 import LGSemiModalNavController
+import Popover
 
 class MarketPlaceV2ViewController: UIViewController {
 
     @IBOutlet weak var btn_sortby: UIButton!
     @IBOutlet weak var btn_filter: UIButton!
     @IBOutlet weak var view_container: UIView!
+    
+    var popover_show = Popover()
+    var popover_filter = Popover()
+    
+    private var popoverOptions: [PopoverOption] = [
+        .Type(.Down),
+        .BlackOverlayColor(UIColor(white: 0.0, alpha: 0.6))
+    ]
+    
+    
+    @IBAction func dismiss_pop(sender: AnyObject) {
+        popover_filter.dismiss()
+        popover_show.dismiss()
+    }
+    var test = true
+    
+    var clicked_button = false
+    
+    private var texts_sortby = ["Highest to Lowest Price", "Lowest to Highest Price", "Earliest Start Date"]
 
     
     //@IBOutlet weak var leftBottomButton: UIButton!
@@ -43,6 +63,26 @@ class MarketPlaceV2ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sortv1_Press(sender: AnyObject) {
+        
+        //clicked_button = true
+        
+        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 40, height: 140))
+        
+        let startPoint = CGPoint(x: self.view.frame.width/2 , y: 100)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.scrollEnabled = false
+        self.popover_show = Popover(options: self.popoverOptions, showHandler: nil, dismissHandler: nil)
+        
+        //var popover = Popover()
+        self.popover_show.show(tableView, point: startPoint)
+        
+    }
+    
+    
     @IBAction func on_Press(sender: AnyObject) {
         var lgVC = UIViewController();
         
@@ -68,6 +108,7 @@ class MarketPlaceV2ViewController: UIViewController {
         self.presentViewController(semiModal, animated: true, completion: { _ in })
         
     }
+    
     @IBAction func on_Press_filter(sender: AnyObject) {
         var lgVC = UIViewController();
         
@@ -101,5 +142,35 @@ class MarketPlaceV2ViewController: UIViewController {
     }
 
     
+}
+
+extension MarketPlaceV2ViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.popover_show.dismiss()
+        clicked_button = false
+        
+        var controller = self.childViewControllers[0] as! MarketPlaceV2TableViewController
+        controller.sort = indexPath.row
+        controller.loadObjects()
+        controller.viewWillAppear(true)
+    }
+}
+
+extension MarketPlaceV2ViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        //        if(clicked_button == true){
+        return texts_sortby.count
+        //        } else {
+        //            return 1
+        //        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel?.text = self.texts_sortby[indexPath.row]
+        return cell
+    }
 }
 
